ff6c1d81b2cf42a22146ccd3d795dd41a61a9d3c
sangeetahugar032 <sangeetahugar032@gmail.com>
2026-01-12 13:27:15 +0530

    Add files via upload

---
+++ Ass no1.Online Order Summary Generator.py
@@ -0,0 +1,5 @@
+item_prices=[499.99,1299.50,249.75,89.99]
+total_order_value=sum(item_prices)
+print("Order Summary")
+print("items prices:(item_prices)")
+print("Total Order Values:(total_order_value:2)")

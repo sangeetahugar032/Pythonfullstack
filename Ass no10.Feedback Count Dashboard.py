75ba18bb1b14d7f0cd78a99fa498c0acd3541759
sangeetahugar032 <sangeetahugar032@gmail.com>
2026-01-12 13:32:46 +0530

    Add files via upload

---
+++ Ass no10.Feedback Count Dashboard.py
@@ -0,0 +1,7 @@
+feedback = {"Positive":45,"Neutral":18,"Negative":7}
+total = feedback["Positive"]+feedback["Neutral"]+feedback["Negative"]
+print("Feedback Summary:")
+print("Positive:",feedback["Positive"])
+print("Neutral:",feedback["Neutral"])
+print("Negative:",feedback["Negative"])
+print("\nTotal Feedback:",total)
